c981163094eaf3f5597d8904ab710c6acefa677d
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-12 00:59:10 +0530

    ability to add queue

---
+++ src/StacksQueues/Queue.java
@@ -0,0 +1,13 @@
+package StacksQueues;
+
+public class Queue {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLinkedList(5);
+        list.addLinkedList(10);
+        list.addLinkedList(15);
+        list.addLinkedList(20);
+        list.diaplay();
+
+    }
+}
